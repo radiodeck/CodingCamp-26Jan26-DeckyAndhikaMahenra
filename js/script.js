931c82dda9dfdf928d75741c8ab5aecf5fd9ec4f
radiodeck <decky.a@mail.ugm.ac.id>
2026-01-29 22:28:23 +0700

    Mini Project Configuration

---
+++ js/script.js
@@ -0,0 +1,7 @@
+welcomeMessage();
+
+function welcomeMessage() {
+    const userResponse = prompt("Welcome to Decky Website! Hope you have a great experience here.");
+    console.log("User Response:", userResponse);
+
+}
